e250a33c4d720e1607902bfc0278d42f0a5705a2
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-06-06 15:26:02 +0200

    config test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Album.destroy_all
+Photo.destroy_all
+
+puts 'Creating 5 photos...'
+5.times do |i|
+  photo = Photo.create!(
+    url: Faker::Company.name
+  )
+  puts "#{i + 1}. #{photo.name}"
+end
+puts 'Finished!'
