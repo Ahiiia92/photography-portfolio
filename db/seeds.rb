15f203e1f76683e309b05356e3325c59db379533
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-06-07 15:31:38 +0200

    added cover to the album seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 require 'faker'
+require "open-uri"
 
+puts 'Deleting all Albums'
 Album.destroy_all
 
 puts 'Creating 5 Albums'
-5.times do |i|
+10.times do |i|
+  file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
   album = Album.create!(
     title: Faker::Book.title,
     description: Faker::Lorem.paragraph,
@@ -12,6 +15,8 @@ puts 'Creating 5 Albums'
     date: Faker::Date.between_except(from: 4.year.ago, to: 1.year.from_now, excepted: Date.today),
     quote: Faker::Quote.yoda
   )
+  album.cover.attach(io: file, filename: "#{album.title}.png", content_type: 'image/png')
+
   puts "#{i + 1}. #{album.title} created"
 end
 puts "Albums ==> Done"
