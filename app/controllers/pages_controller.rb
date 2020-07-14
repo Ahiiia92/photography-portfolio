262816a3664ed03c10fbd0b352bea3a8053e55c2
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-07-14 19:30:09 +0200

    dynamic carousel initialize

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :about, :contact]
 
   def home
+    # @albums = Album.all
+    @album = Album.find(33)
+    # @cover = @album.cover
   end
 
   def about
